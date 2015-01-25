4e5b2543083869eb40cf309b9863575b258273f9
Joakim Engstrom <engstrom.joakim@gmail.com>
2015-01-25 17:54:44 +0100

    First draft of my personal landing page

---
+++ js/switches.js
@@ -0,0 +1,11 @@
+﻿function switcher(elem) {
+	document.getElementById('about').style.display = 'none';
+	document.getElementById('links').style.display = 'none';
+	document.getElementById(elem).style.display = 'inline';
+	if (elem == 'about') {
+		triangle_margin = '10px 54px 0 0';
+	} else if (elem == 'links') {
+		triangle_margin = '10px 160px 0 0';
+	}
+	document.getElementById('triangle').style.margin = triangle_margin;
+}
