9e27a0f466c8439d25f9cb3bbe59510c2bee7a8c
Joakim Engstrom <engstrom.joakim@gmail.com>
2015-02-18 21:19:23 +0100

    [Information] Seperated portfolio into it's own section

--- js/switches.js
+++ js/switches.js
@@ -1,11 +1,23 @@
 ﻿function switcher(elem) {
 	document.getElementById('about').style.display = 'none';
 	document.getElementById('links').style.display = 'none';
+	document.getElementById('apps_portfolio').style.display = 'none';
 	document.getElementById(elem).style.display = 'inline';
-	if (elem == 'about') {
-		triangle_margin = '10px 54px 0 0';
-	} else if (elem == 'links') {
-		triangle_margin = '10px 160px 0 0';
+
+	switch (elem) {
+
+		case 'about':
+			triangle_margin = '10px 54px 0 0';
+		break;
+
+		case 'links':
+        	triangle_margin = '10px 160px 0 0';
+        break;
+
+       	case 'apps_portfolio':
+             triangle_margin = '10px 270px 0 0';
+		break;
 	}
+
 	document.getElementById('triangle').style.margin = triangle_margin;
 }
